6552c68ce353deac4e1a39fcfbf2000b2134a386
Sean Stowers <sean.stowers@rackspace.com>
2018-04-08 11:16:04 -0500

    tests pass

--- lib/address.rb
+++ lib/address.rb
@@ -2,4 +2,31 @@ require_relative 'geocoding'
 
 class Address
   attr_accessor :lat, :lng, :full_address
+
+  def initialize(lat: nil, lng: nil, full_address: nil)
+    @lat = lat
+    @lng = lng
+    @full_address = full_address
+    full_address.nil? ? set_address : set_lat_lng
+  end
+
+  def geocoded?
+    !(lat.nil? || lng.nil?)
+  end
+
+  def reverse_geocoded?
+    !full_address.nil?
+  end
+
+  private
+
+  def set_address
+    return if reverse_geocoded?
+    @full_address = Geocoder.address([lat, lng])
+  end
+
+  def set_lat_lng
+    return if geocoded?
+    @lat, @lng = Geocoder.coordinates(full_address)
+  end
 end
--- spec/lib/address_spec.rb
+++ spec/lib/address_spec.rb
@@ -3,24 +3,32 @@ RSpec.describe Address do
   let(:lat) { 40.181306 }
   let(:lng) { -80.265949 }
 
-  subject(:address) { described_class.new }
-
   describe 'geocoding' do
-    let(:payload) {{  'longt' => lng, 'latt' => lat }}
-    let(:result) { [ double(data: payload) ] }
+    subject(:address) { described_class.new(full_address: full_address) }
+
+    let(:result) { [lat, lng] }
 
     it 'geocodes with Geocoder API' do
-      expect(Geocoder).to receive(:search).with(full_address).and_return result
+      expect(Geocoder).to receive(:coordinates).with(full_address).and_return result
+      address
     end
 
     it 'is geocoded' do
       expect(address).to be_geocoded
     end
+
+    it 'does not fetch coordinates when lat and lng are present' do
+      allow(address).to receive(:geocoded?).and_return(true)
+      expect(Geocoder).to receive(:coordinates).never
+      address
+    end
   end
 
   describe 'reverse geocoding' do
+    subject(:address) { described_class.new(lat: lat, lng: lng) }
+
     let :payload do
-      {   
+      {
         'usa'=> {
           'uscity' => 'WASHINGTON',
           'usstnumber' => '1',
@@ -30,15 +38,22 @@ RSpec.describe Address do
         }
       }
     end
-    
+
     let(:result) { [ double(data: payload) ] }
 
     it 'reverse geocodes with Geocoder API' do
-      expect(Geocoder).to receive(:search).with("#{lat},#{lng}").and_return result
+      expect(Geocoder).to receive(:address).with([lat, lng]).and_return result
+      address
     end
 
     it 'is reverse geocoded' do
       expect(address).to be_reverse_geocoded
+    end
+
+    it 'does not fetch address when address is present' do
+      allow(address).to receive(:reverse_geocoded?).and_return(true)
+      expect(Geocoder).to receive(:address).never
+      address
     end
   end
 
